8825c7ad741f473767c5c53a40aa7f5d24d97ca8
tengvalk <tengvalk@arcada.fi>
2025-01-10 09:57:44 +0200

    Initial commit

---
+++ src/Vehicle.java
@@ -0,0 +1,17 @@
+public class Vehicle {
+
+    // Deklarerar String variabeln name
+    String name;
+    String type;
+
+    public String getName() {
+        return name;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
