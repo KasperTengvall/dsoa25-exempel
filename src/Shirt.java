d71fe7786eafac0d6e5715394f3bf037d7b13b11
tengvalk <tengvalk@arcada.fi>
2025-01-10 10:25:33 +0200

    Code-Challenge lektion 2

---
+++ src/Shirt.java
@@ -0,0 +1,10 @@
+public class Shirt extends Clothing{
+
+    public Shirt(String name) {
+        super.name = name;
+        super.type = "T-Shirt";
+        super.color = "blue";
+        super.brand = "Levis";
+        super.price = "29.99€";
+    }
+}
