8eeea51c4f8bd79e7ca99607135eafdab6cf4fe8
tengvalk <tengvalk@arcada.fi>
2025-01-20 11:51:29 +0200

    Lektion 5 CC

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello java!");
+        int myInt = Utils.getIntInput();
+        System.out.println("Du gav heltalet: " + myInt);
     }
 }
