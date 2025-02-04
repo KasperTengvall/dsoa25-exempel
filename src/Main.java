2e8aecf93e3b83637e35aed43ccfea6c5cba4427
tengvalk <tengvalk@arcada.fi>
2025-02-04 11:23:46 +0200

    Lektion 8 CC

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
-
 public class Main {
-
-
     public static void main(String[] args) {
+        // Example data
+        String[] sortedData = {"Aragorn", "Arwen", "Balin", "Bilbo", "Boromir", "Celeborn",
+                "Denethor", "Elrond", "Eowyn", "Faramir", "Frodo", "Galadriel",
+                "Gandalf", "Gildor", "Glorfindel", "Gimli", "Gollum", "Haldir",
+                "Legolas", "Luthien", "Merry", "Pippin", "Radagast", "Rosie",
+                "Sam", "Saruman", "Theoden", "Thorin", "Treebeard"};
 
-        new BigO();
+        StringArray names = new StringArray(sortedData);
 
+        names.add("Frodo");
+        System.out.println(names.toString());
+
+        names.reverse();
+        System.out.println(names.toString());
+
+        System.exit(0);
     }
 }
